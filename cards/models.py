e157b0bb599fe445a35ef934d96c696e36bd87e8
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-16 16:59:40 -0300

    Criada models de Cartão e Lote

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Lote(models.Model):
+    lote = models.CharField(max_length=8)
+    qtd_registros = models.IntegerField()
+
+
+class Cartao(models.Model):
+    identificador_linha = models.CharField(max_length=1)
+    numeracao_lote = models.CharField(max_length=6)
+    numero_cartao = models.CharField(max_length=18)
+    lote = models.ForeignKey(Lote, on_delete=models.PROTECT)
