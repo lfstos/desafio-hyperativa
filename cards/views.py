264fbb67bf63007cfa7374d196c78b02b87b4f03
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-16 17:02:32 -0300

    Criada Views de Cartão, Consulta Cartão e Criação e Listagem de Lotes

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import logging
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Lote, Cartao
+from .serializers import LoteSerializer, CartaoSerializer
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+class CartaoListCreateView(generics.ListCreateAPIView):
+    queryset = Cartao.objects.all()
+    serializer_class = CartaoSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ConsultarCartaoView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, numero_cartao):
+        try:
+            cartao = Cartao.objects.get(numero_cartao=numero_cartao)
+            return Response({'identificador_sistema': cartao.id})
+        except Cartao.DoesNotExist:
+            return Response({'error': 'Cartão não encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class LoteListCreateView(generics.ListCreateAPIView):
+    queryset = Lote.objects.all()
+    serializer_class = LoteSerializer
